71552cf527e537c444e7e2b6cbe84c415ad73970
Juliana <29216050+jba93@users.noreply.github.com>
2017-11-17 10:56:47 -0200

    Correção da identação

--- Referência Bibliográfica.c
+++ Referência Bibliográfica.c
@@ -3,125 +3,128 @@
 #include <stdlib.h>
 #include <locale.h>
 
-char formatanome(char nome[50], char ref[20]){
-     char letra1, letra2;
-     int tam=0, tam_ref=0, i=0, j=0, cont=0, achou=0, pos;
-     tam = strlen(nome);
-     strupr(nome); //transforma todo o nome digitado em letras maiúsculas
-    //retira as palavras de ligação (e, de, do, da, dos, das, la, di, del) e também retira "Junior" e "Neto", se houver
-     while (i<tam)
-	 {
-           if (nome[i]==' ' && nome[i+1]=='E' && nome[i+2]==' ') //se encontrar um "E". Ex: João Silva e Silva
-		   {
-		   		j = i;
-               	while (j<tam)
-			   	{
-                    nome[j+1]=nome[j+3];
-                    j++;
-               	}
-               tam = strlen(nome);
-           }
+char formatanome(char nome[50], char ref[20])
+{
+	char letra1, letra2;
+     	int tam=0, tam_ref=0, i=0, j=0, cont=0, achou=0, pos;
+     	tam = strlen(nome);
+     	strupr(nome); //transforma todo o nome digitado em letras maiÃºsculas
+    	//retira as palavras de ligaÃ§Ã£o (e, de, do, da, dos, das, la, di, del) e tambÃ©m retira "Junior" e "Neto", se houver
+    	while (i<tam)
+	{
+		if (nome[i]==' ' && nome[i+1]=='E' && nome[i+2]==' ') //se encontrar um "E". Ex: JoÃ£o Silva e Silva
+		{
+			j = i;
+               		while (j<tam)
+			{
+				nome[j+1]=nome[j+3];
+				j++;
+			}
+               		tam = strlen(nome);
+           	}
         
 	        if (nome[i]==' ' && nome[i+1]=='D' && (nome[i+2]=='A' || nome[i+2]=='E' || nome[i+2]=='I' || nome[i+2]=='O' || nome[i+2]=='U') && nome[i+3]==' ') //DA, DE, DI, DO, DU
-			{
-				j=i;
+		{
+			j=i;
 	           	while (j<tam)
-			   	{
-				   nome[j+1]=nome[j+4];
-				   j++;
-	            }
-	            tam = strlen(nome);
+			{
+				nome[j+1]=nome[j+4];
+				j++;
+	            	}
+	            	tam = strlen(nome);
 	        }
 	        
 	        if (nome[i]==' ' && nome[i+1]=='L' && nome[i+2]=='A' && nome[i+3]==' ') //LA
+		{
+			j = i;
+               		while (j<tam)
 			{
-				j = i;
-               	while (j<tam)
-			   	{
-	                nome[j+1]=nome[j+4];
-	                j++;
+	                	nome[j+1]=nome[j+4];
+	                	j++;
 	           	}
 	           	tam = strlen(nome);
 	        }
 	        
 	        if (nome[i]==' ' && nome[i+1]=='D' && (nome[i+2]=='A' || nome[i+2]=='O')&& nome[i+3]=='S' && nome[i+4]==' ') //DAS, DOS
-			{
-				j=i;
+		{
+			j=i;
 	           	while (j<tam)
-			   	{
-		          nome[j+1]=nome[j+5];
-		          j++;
+			{
+				nome[j+1]=nome[j+5];
+				j++;
 		      	}
 		    	tam = strlen(nome);
-		    }
+		}
 		    
 	        if (nome[i]==' ' && nome[i+1]=='D' && nome[i+2]=='E' && nome[i+3]=='L' && nome[i+4]==' ')//DEL
-			{
-	           	j=i;
+		{
+			j=i;
 	           	while (j<tam)
-			   	{
-				   nome[j+1]=nome[j+5];
-				   j++;
-	            }
-	            tam = strlen(nome);
+			{
+				nome[j+1]=nome[j+5];
+				j++;
+	            	}
+	            	tam = strlen(nome);
 	        }
 	        
-	        if (nome[i]==' ' && nome[i+1]=='J' && (nome[i+2]=='U' || nome[i+2]=='Ú') && nome[i+3]=='N' && nome[i+4]=='I' && nome[i+5]=='O' && nome[i+6]=='R')//JUNIOR
-			{
-				for (j=0; j<7; j++)
-					nome[i+j] = '\0';
-				tam = strlen(nome);
+	        if (nome[i]==' ' && nome[i+1]=='J' && (nome[i+2]=='U' || nome[i+2]=='Ãš') && nome[i+3]=='N' && nome[i+4]=='I' && nome[i+5]=='O' && nome[i+6]=='R')//JUNIOR
+		{
+			for (j=0; j<7; j++)
+			nome[i+j] = '\0';
+			tam = strlen(nome);
 	        }
 	        
 	        if (nome[i]==' ' && nome[i+1]=='N' && nome[i+2]=='E' && nome[i+3]=='T' && nome[i+4]=='O')//NETO
-			{
-				for (j=0; j<5; j++)
-					nome[i+j] = '\0';
-				tam = strlen(nome);
+		{
+			for (j=0; j<5; j++)
+			nome[i+j] = '\0';
+			tam = strlen(nome);
 	        }
-	        i++; //o próximo caractere do nome será analisado 
-    }
+	        i++; //o prÃ³ximo caractere do nome serÃ¡ analisado 
+    	}
     
-    tam=strlen(nome); //tamanho do nome depois da retirada dos sobrenomes de ligação
+    	tam=strlen(nome); //tamanho do nome depois da retirada dos sobrenomes de ligaÃ§Ã£o
     
-    while (achou == 0)
+    	while (achou == 0)
+	{
+		letra1 = nome[i]; // comeÃ§a a procurar o Ãºltimo sobrenome comeÃ§ando pelo Ãºltimo caracter
+      		if (letra1 == ' ')
+	  	{ 
+        		pos = i;
+        		achou = 1;
+      		}
+		
+      		else
+	 		i--;
+   	}
+    
+    	for (i=pos+1; i<tam; i++) // coloca o Ãºltimo sobrenome em ref
 	{ 
-      letra1 = nome[i]; // começa a procurar o último sobrenome começando pelo último caracter
-      if (letra1 == ' ')
-	  { 
-        pos = i;
-        achou = 1;
-      }
-      else
-	 	i--;
-    }
+      		ref[cont] = nome[i];
+      		cont++;
+    	}
+	
+    	ref[cont] = ','; //seguido por uma vÃ­rgula e por um espaÃ§o em branco
+    	cont++;
+    	ref[cont] = ' '; 
+    	cont++;
     
-    for (i=pos+1; i<tam; i++) // coloca o último sobrenome em ref
-	{ 
-      ref[cont] = nome[i];
-      cont++;
-    }
-    ref[cont] = ','; //seguido por uma vírgula e por um espaço em branco
-    cont++;
-    ref[cont] = ' '; 
-    cont++;
-    
-    letra2=nome[0]; //letra2 recebe a primeira letra do primeiro nome da pessoa
-    for (i=0; i<=pos; i++) //i vai do início de nome até o último caracter antes do último nome
-	{ 
-      letra1 = nome[i]; 
-      if ((letra1 == ' ') || (letra1 == '\0'))
-	  { 
-       ref[cont] = letra2;
-	   cont++;
-	   ref[cont]='.';
-	   cont++;
-	   letra2 = nome[i+1];
-	 }
-    }
-    cont++;
-    ref[cont]=' ';
-    tam_ref = strlen(ref);
+	letra2=nome[0]; //letra2 recebe a primeira letra do primeiro nome da pessoa
+	for (i=0; i<=pos; i++) //i vai do inÃ­cio de nome atÃ© o Ãºltimo caracter antes do Ãºltimo nome
+	{
+      		letra1 = nome[i]; 
+      		if ((letra1 == ' ') || (letra1 == '\0'))
+	  	{ 
+       			ref[cont] = letra2;
+	   		cont++;
+	   		ref[cont]='.';
+	   		cont++;
+	   		letra2 = nome[i+1];
+	 	}
+    	}
+    	cont++;
+    	ref[cont]=' ';
+    	tam_ref = strlen(ref);
   	return ref[20];
 }  
 
@@ -132,19 +135,19 @@ main()
 	while (op==1)
 	{
 		setlocale(LC_ALL, "Portuguese");
-	    char nome[50], ref[20];
-	    int x;
-	    for (x=0; x<21; x++)
-	    	ref[x]=' ';
-	    int i;
-	    printf("\n\n\nDigite o nome completo a ser formatado: "); 
-	    fflush(stdin); //limpar o buffer
-	    gets(nome);
-	    formatanome(nome, ref); //chama a segunda função
-	    fflush(stdin);
-	    printf("Nome em referência bibliográfica: %s\n\n\n", ref);
-	    printf ("Digite 1 para formatar outro nome: ");
-	    scanf ("%d", &op);
+		char nome[50], ref[20];
+		int x;
+		for (x=0; x<21; x++)
+			ref[x]=' ';
+		int i;
+		printf("\n\n\nDigite o nome completo a ser formatado: "); 
+		fflush(stdin); //limpar o buffer
+		gets(nome);
+		formatanome(nome, ref); //chama a segunda funÃ§Ã£o
+		fflush(stdin);
+		printf("Nome em referÃªncia bibliogrÃ¡fica: %s\n\n\n", ref);
+		printf ("Digite 1 para formatar outro nome: ");
+		scanf ("%d", &op);
 	}
-    exit(0);
+    	exit(0);
 }
